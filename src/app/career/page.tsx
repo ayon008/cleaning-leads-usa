3e66e62c547a116126fb923c0a4733cd85dcaa28
ayon008 <shariar.ayon128@gmail.com>
2025-09-23 14:59:51 +0600

    page-level-done

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Hero from "../Shared/Banner/Hero";
 
-const page = () => {
+const Page = () => {
   return (
     <section id="career">
-      <Hero title="" headline="" des={<></>} />
+      <Hero
+        title="Careers at Cleaning Leads USA"
+        headline="Join Our Growing Team"
+        des={
+          <>
+            Explore exciting career opportunities with{" "}
+            <strong>Cleaning Leads USA</strong>. We’re always looking for
+            motivated individuals to help us connect cleaning businesses with
+            verified leads. Grow your career with us and be part of a
+            supportive, innovative team.
+          </>
+        }
+      />
+      <div className="py-20 container">
+        <h2 className="text-3xl text-secondary font-semibold">No vacancy right now </h2>
+      </div>
     </section>
   );
 };
 
-export default page;
+export default Page;
