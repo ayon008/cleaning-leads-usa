8730d1cce43adafe62732082b816217fc0d37c07
ayon008 <shariar.ayon128@gmail.com>
2025-09-26 22:54:21 +0600

    loading-added

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="h-dvh !z-[1000] w-full justify-center flex items-center bg-white">
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default loading;
