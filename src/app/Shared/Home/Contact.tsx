027f0f7b1b9163fe62203212ede3debc6b9b067f
ayon008 <shariar.ayon128@gmail.com>
2025-10-23 13:48:59 +0600

    added

--- src/app/Shared/Home/Contact.tsx
+++ src/app/Shared/Home/Contact.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
       itemType="http://schema.org/ContactPage"
     >
       <div className="inset-0 absolute bg-tertiary opacity-80 z-10" aria-hidden="true"></div>
-      <div className="container h-full flex md:flex-row flex-col items-center justify-between md:gap-20 gap-10 relative z-20" itemScope itemType="http://schema.org/Organization">
+  <div className="container h-full flex md:flex-row flex-col items-center justify-between md:gap-20 gap-10 relative z-20">
         <div className="md:w-1/2 w-full">
           <p className="text-secondary font-semibold md:text-base text-sm">Contact Us Today</p>
           <h2 id="contact-headline" className={`text-white md:text-4xl text-3xl font-bold mt-2 mb-5`} itemProp="headline">
@@ -32,14 +32,11 @@ const Contact = () => {
               title="Call Cleaning Leads USA"
               className="flex items-center gap-2 bg-secondary py-3 px-4 rounded-4xl w-fit mt-6"
               id="cta"
-              itemProp="contactPoint"
-              itemScope
-              itemType="http://schema.org/ContactPoint"
             >
               <Headphones className="text-white" size="2rem" aria-hidden="true" />
               <div>
                 <p className="text-sm text-white font-semibold">Phone Number</p>
-                <p className="text-white text-base font-bold cursor-pointer" itemProp="telephone">
+                <p className="text-white text-base font-bold cursor-pointer">
                   +1 (332) 600-4766
                 </p>
               </div>
